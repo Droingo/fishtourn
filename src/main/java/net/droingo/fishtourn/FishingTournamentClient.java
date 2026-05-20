971ae88de649c2db16c16f65a702ee7dc997520b
Droingo <cmbmx57@gmail.com>
2026-05-20 17:10:40 +1000

    Initial commit

---
+++ src/main/java/net/droingo/fishtourn/FishingTournamentClient.java
@@ -0,0 +1,11 @@
+package net.droingo.fishtourn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishingTournamentClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/droingo/fishtourn/FishingTournamentDataGenerator.java
@@ -0,0 +1,11 @@
+package net.droingo.fishtourn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FishingTournamentDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
